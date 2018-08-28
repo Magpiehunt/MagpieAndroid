935afcb6eeb717de92e2f8f709d9da71eacd7fad
ianmtaylor <tayloian000@gmail.com>
2018-08-27 17:13:18 -0700

    Qr code and Maps bugs fixed

--- MagpieHunt/app/src/main/java/com/davis/tyler/magpiehunt/Activities/ActivitySignIn.java
+++ MagpieHunt/app/src/main/java/com/davis/tyler/magpiehunt/Activities/ActivitySignIn.java
@@ -1,10 +1,16 @@
 package com.davis.tyler.magpiehunt.Activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.davis.tyler.magpiehunt.Fragments.FragmentSigninInitial;
 import com.davis.tyler.magpiehunt.Fragments.FragmentWalkthroughContainer;
@@ -12,10 +18,17 @@ import com.davis.tyler.magpiehunt.R;
 
 public class ActivitySignIn extends AppCompatActivity implements FragmentSigninInitial.FragmentSigninInitialListener{
     private FragmentSigninInitial mFragmentSigninInitial;
+    private static final String TAG = "Actvity_signin";
+
+    private static final String FINE_LOCATION = android.Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+    private boolean mLocationPermissionGranted;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+        getLocationPermission();
         getSupportActionBar().setBackgroundDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.action_bar_gradient, null));
         mFragmentSigninInitial = new FragmentSigninInitial();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
@@ -32,5 +45,44 @@ public class ActivitySignIn extends AppCompatActivity implements FragmentSigninI
         ft.replace(R.id.currentfragment, new FragmentWalkthroughContainer());
         ft.addToBackStack(null);
         ft.commit();
+    }
+
+    private void getLocationPermission(){
+        String[] permissions = {FINE_LOCATION, COURSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGranted = true;
+                Log.e(TAG, "init map from getLocationPermission()");
+            }
+            else{
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+
+        }
+        else{
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted = false;
+        switch (requestCode) {
+            case LOCATION_PERMISSION_REQUEST_CODE: {
+                if (grantResults.length > 0) {
+                    for (int i = 0; i < grantResults.length; i++) {
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                            mLocationPermissionGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionGranted = true;
+                    //initialize our map
+                    Log.e(TAG, "init map from onRequestPermissionResult()");
+
+                }
+            }
+        }
     }
 }
